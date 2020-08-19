374205b26d31f5841478c07094bb16949937c09f
Samad Mahmood <msamadm99@gmail.com>
2020-08-20 04:18:19 +0500

    Grid View Work in WAD with Csharp

---
+++ DataGridView-WAD/DataGridView-WAD/DBManagerClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridView_WAD
+{
+    class DBManagerClass
+    {
+        public static DataTable GetTestData()
+        {
+            // Create table with name 'Test'
+            DataTable tbl = new DataTable("Test");
+
+            //Make coulmn with name "ID"
+            DataColumn dc = new DataColumn("ID", typeof(System.Int32));
+            
+            //  Add "ID" column
+            tbl.Columns.Add(dc);
+
+            //Make coulmn with name "Name"
+            dc = new DataColumn("Name", typeof(System.String));
+
+            //  Add "Name" column
+            tbl.Columns.Add(dc);
+
+            //Make coulmn with name "Age"
+            dc = new DataColumn("Age", typeof(System.Int32));
+
+            //  Add "Age" column
+            tbl.Columns.Add(dc);
+
+
+            // Create Row with name 'dr'
+            DataRow dr = tbl.NewRow();
+
+            //Add data in first row
+            dr["ID"] =1;
+            dr["Name"] ="Samad";
+            dr["Age"] =20;
+
+            //Add row in table
+            tbl.Rows.Add(dr);
+
+            // Create 2nd Row
+            dr = tbl.NewRow();
+
+            //Add data in second row
+            dr["ID"] = 2;
+            dr["Name"] = "Not Samad";
+            dr["Age"] = 30;
+
+            //Add row in table
+            tbl.Rows.Add(dr);
+
+            return tbl;
+        }
+    }
+}
---
+++ DataGridView-WAD/DataGridView-WAD/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGridView_WAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = DBManagerClass.GetTestData();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3)
+            {
+                var id = (int)dataGridView1.CurrentRow.Cells[0].Value;
+                Form2 f2 = new Form2(id);
+                f2.ShowDialog();
+            }
+            if (e.ColumnIndex == 4)
+            {
+                
+            }
+        }
+    }
+}
